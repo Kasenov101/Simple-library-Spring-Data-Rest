f28fef881c80087a698e5cfc6d360ec5657cbf70
Kasenov Abylay <kasenov101@gmail.com>
2022-12-27 20:50:26 +0600

    created interface CommonService

---
+++ src/main/java/com/kasenov/libpro/simplelibrary/Service/CommonService.java
@@ -0,0 +1,21 @@
+package com.kasenov.libpro.simplelibrary.Service;
+
+import com.kasenov.libpro.simplelibrary.Entity.AbstractEntity;
+import com.kasenov.libpro.simplelibrary.ExceptionHandler.CannotRemoveException;
+import com.kasenov.libpro.simplelibrary.ExceptionHandler.CannotSaveException;
+import com.kasenov.libpro.simplelibrary.ExceptionHandler.NotFoundException;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CommonService<E extends AbstractEntity> {
+    List<E> getAll() throws NotFoundException;
+
+    E getById(long id) throws NotFoundException;
+
+    ResponseEntity<E> save(E entity) throws CannotSaveException;
+
+    ResponseEntity<E> update(E entity) throws NotFoundException, CannotSaveException;
+
+    ResponseEntity<Long> delete(long id) throws NotFoundException, CannotRemoveException;
+}
