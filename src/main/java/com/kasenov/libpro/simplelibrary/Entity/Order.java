872e5fd42bd74c350b6849ebb9804aeec1758f10
Kasenov Abylay <kasenov101@gmail.com>
2023-01-04 18:27:51 +0600

    was added annotation @JsonIdentityInfo to avoid infinite recursion exception

--- src/main/java/com/kasenov/libpro/simplelibrary/Entity/Order.java
+++ src/main/java/com/kasenov/libpro/simplelibrary/Entity/Order.java
@@ -1,6 +1,8 @@
 package com.kasenov.libpro.simplelibrary.Entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,9 @@ import java.util.List;
 @Table(name = "orders")
 @Getter
 @Setter
+@JsonIdentityInfo(
+        scope = Book.class, generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Order extends AbstractEntity{
 
     @ManyToOne
