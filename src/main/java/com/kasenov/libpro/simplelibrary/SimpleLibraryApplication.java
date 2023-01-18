3c99c32276d0ae19929ccf7790149af237def170
Kasenov Abylay <kasenov101@gmail.com>
2023-01-18 17:44:00 +0600

    Added method for creating, and config modelMapper

--- src/main/java/com/kasenov/libpro/simplelibrary/SimpleLibraryApplication.java
+++ src/main/java/com/kasenov/libpro/simplelibrary/SimpleLibraryApplication.java
@@ -1,10 +1,29 @@
 package com.kasenov.libpro.simplelibrary;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleLibraryApplication {
+
+    private static ModelMapper modelMapper;
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        if (modelMapper == null) {
+            modelMapper = new ModelMapper();
+        }
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+        return modelMapper;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleLibraryApplication.class, args);
